8752a98d2edac2ea63ab3f23effac0bbc5ff6322
Bimer2350 <aliimer4666@icloud.com>
2024-10-08 22:05:00 +0300

    Update hesapm.js

--- Java script/hesapm.js
+++ Java script/hesapm.js
@@ -3,19 +3,21 @@ alert("Bir bölüme değer girmediğiniz zaman 0(Sıfır) olarak alınır")
 let bos = "\n\r";
 
 let sayi = prompt("Lüfen bir sayı giriniz.");
-let islem = prompt("Yapacağınız işlemi giriniz."+bos+"*"+bos+"/"+bos+"+"+bos+"-");
+let islem = prompt("Yapacağınız işlemi giriniz."+bos
+    +"+    :    -    :    *    :     /    "+bos+"**"
+);
 let sayi2 = prompt("Lüfen bir sayı giriniz.");
 
 
+document.write("Sonuç : ")
 
 
-let t = "Sonuç : "
 
 if(islem == "*"){
-    document.write( t+Number(sayi)*Number(sayi2))
+    document.write( Number(sayi)*Number(sayi2))
 }
 else if(islem == "/"){
-    document.write( t+Number(sayi)/Number(sayi2))
+    document.write( Number(sayi)/Number(sayi2))
 }
 else if(islem == "-"){
     document.write( Number(sayi)-Number(sayi2))
@@ -24,9 +26,13 @@ else if(islem == "-"){
         
         document.write( Number(sayi)+Number(sayi2))
         }
+        else if(islem =="**"){
+            document.write(sayi**=sayi2)
+        }
         else{
             document.write("Hatalı İşlem girdiniz.")
         }
+            
             
         
      
